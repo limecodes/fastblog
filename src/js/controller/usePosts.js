4ef62fdc3081d6b4002cc47c378e174ac372c431
limecodes <yan.shcherbakov+limecodes@gmail.com>
2023-06-26 11:09:10 +0300

    feat: Added usePosts controller.

---
+++ src/js/controller/usePosts.js
@@ -0,0 +1,41 @@
+import { createState } from '../lib/createState.js'
+import { getUrl } from '../lib/getUrl.js'
+import { fetchPaginated } from '../lib/fetchPaginated.js'
+
+export function usePosts() {
+  const posts = createState([])
+  const loading = createState(false)
+  const error = createState(null)
+  // The initial next link is the first page
+  const nextLink = createState(getUrl('/posts'))
+
+  async function fetchPosts(urlObject) {
+    loading.set(true)
+    
+    const { posts: paginatedPosts, next } = await fetchFromNetwork(urlObject)
+
+    if (paginatedPosts.length) {
+      loading.set(false)
+      nextLink.set(next ? new URL(next) : null)
+      
+      return paginatedPosts
+    } else {
+      loading.set(false)
+      return []
+    }
+  }
+
+  async function fetchFromNetwork(urlObject) {
+    return await fetchPaginated(urlObject)
+  }
+
+  fetchPosts(nextLink.get()).then((paginatedPosts) => {
+    posts.set(paginatedPosts)
+  })
+
+  return {
+    posts,
+    loading,
+    error,
+  }
+}
---
+++ src/js/posts.js
@@ -0,0 +1,5 @@
+import { usePosts } from './controller/usePosts.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const { posts } = usePosts()
+})

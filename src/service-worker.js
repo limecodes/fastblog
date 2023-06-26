824b0048f5f052f7b14e494b4f36b2a5df8cfe59
limecodes <yan.shcherbakov+limecodes@gmail.com>
2023-06-26 13:32:18 +0300

    feat: Setup service worker.

---
+++ src/service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'v1'
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/posts.html',
+  '/audio.html',
+  '/css/reset.css',
+  '/css/main.css',
+  '/css/posts.css',
+  '/css/audio.css',
+  '/js/components/Post.js',
+  '/js/components/PostsList.js',
+  '/js/components/Users.js',
+  '/js/controller/usePosts.js',
+  '/js/controller/useUsers.js',
+  '/js/lib/cache.js',
+  '/js/lib/createState.js',
+  '/js/lib/fetchPaginated.js',
+  '/js/lib/getUrl.js',
+  '/js/audio.js',
+  '/js/posts.js',
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(URLS_TO_CACHE))
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) return response
+
+        return fetch(event.request)
+      })
+  )
+})
